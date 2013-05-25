b5a0c31f5d65d939ba17535bed64b7a554ced988
Brandon Murry <brmurry@zappos.com>
2013-05-24 18:27:19 -0700

    simplified function quite a bit

--- bookmarklet/README.md
+++ bookmarklet/README.md
@@ -1,5 +1,5 @@
 #UploadRunner bookmarklet
-### A bookmarklet that calculates absolute monthly totals on Runkeeper
+A bookmarklet that calculates absolute monthly totals on Runkeeper
 
 ##Usage
 Add the content of sumRunMonthBookmarklet-compiled.js to a bookmark. Tips on [adding a bookmarklet](http://www.google.com/search?btnG=1&pws=0&q=adding+a+bookmarklet).
--- bookmarklet/sumRunMonthBookmarklet.js
+++ bookmarklet/sumRunMonthBookmarklet.js
@@ -1,112 +1,70 @@
-(function(w, d){
-  console.clear();
-  var RunkeeperMonthly = function RunkeeperMonthly() {
-    if (!(this instanceof RunkeeperMonthly))
-      return new RunkeeperMonthly();
-
-    var self = this;
-    self.init(); //fire this biatch
-  };
-
+(function(){
   /*
-  * Functionality to loop through and fire all monthly calcs
+  * function for calculating each month
   */
-  RunkeeperMonthly.prototype = {
-    /*
-    * gets all the dom elements per month
-    */
-    getDates: function() {
-      var self = this;
-      var i, actLen, activity;
-
-      for (i = 0, actLen = activities.length; i < actLen; i++) {
-        activity = activities[i] || null;
-        if (activity) {
-          calcRkMonth(activity);
-        }
-      }
-    },
+  function calcRkMonth(activity) {
+    var date;
 
     /*
-    * fires getDates, which collects all dates
+    * gets the api data for each month
     */
-    init: function() {
-      var self = this;
-      self.getDates();
-    }
-  };
+    function getData() {
+      date = activity.dataset.date;
 
-  /*
-  * Class for each month calculation and appending
-  */
-  var calcRkMonth = function calcRkMonth(el) {
-    if (!(this instanceof calcRkMonth))
-      return new calcRkMonth(el);
+      var url = '/activitiesByDateRange?userName=' + user + '&startDate=' + date;
 
-    var self = this;
-
-    self.element = el;
-    self.date = el.dataset.date || null;
-    var splitDate = self.date.split('-');
-    self.month = splitDate[0];
-    self.year = splitDate[2];
-
-    if (self.date) {
-      self.init(el);
-    }
-  };
-
-  calcRkMonth.prototype = {
-    /*
-    * fire getData with data
-    */
-    init: function() {
-      var self = this;
-      self.getData();
-    },
-
-    /*
-    * makes the API call to get info
-    */
-    getData: function() {
-      var self = this;
-      var url = siteLocation + '/activitiesByDateRange?userName=' + user + '&startDate=' + self.date;
-
-      $.getJSON(url, function(r){
-          self.calculate(r.activities[self.year][self.month]);
+      $.getJSON(url, function(response){
+        append(monthTotal(response));
       });
-    },
+    }
 
     /*
     * calculates that month
     */
-    calculate: function(data) {
-      var self = this;
-      var i, dataLen, day, totalDistance = 0;
+    function monthTotal(response) {
+      var i, dataLen, day,
+          totalDistance = 0,
+          split = date.split('-');
 
-      for (i = 0, dataLen = data.length; i < dataLen; i++) {
-        day = data[i];
+      response = response.activities[split[2]][split[0]];
+      for (i = 0, dataLen = response.length; i < dataLen; i++) {
+        day = response[i];
         totalDistance += (+day.distance);
       }
 
-      totalDistance = Math.round(totalDistance*100)/100;
-      self.append(totalDistance);
-    },
+      return Math.round(totalDistance*100)/100;
+    }
 
     /*
     * Appends the calculated monthly total to the page
     */
-    append: function(monthTotal) {
-      var self = this;
-      self.element.innerHTML += '<span style="font-size:10px;font-weight:bold;color:#888">' + monthTotal + 'mi </span>';
+    function append(distance) {
+      activity.innerHTML += '<span style="font-size:10px;font-weight:bold;color:#888">' + distance + 'mi </span>';
     }
-  };
 
-  var activities = $('#activityHistoryMenu').children('div.accordion'),
-      windowLocation = w.location,
-      siteLocation = windowLocation.origin,
-      user = windowLocation.pathname.split('/')[2];
+    /*
+    * Publically accessible methods
+    */
+    return {
+      getData: getData
+    };
+  }
 
-  var rkCalc = RunkeeperMonthly();
+  var user = window.location.pathname.split('/')[2];
 
-}(window, document));
+  (function RunkeeperMonthly() {
+    var activities = document.querySelectorAll('[data-date]'),
+        i, actLen, activity;
+
+    /*
+    * Fires a calculation function for each month
+    */
+    for (i = 0, actLen = activities.length; i < actLen; i++) {
+      activity = activities[i] || null;
+      if (activity) {
+        calcRkMonth(activity).getData();
+      }
+    }
+  }());
+
+}());
